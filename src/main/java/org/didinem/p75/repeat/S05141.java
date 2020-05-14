20105ae0828d69a5648e7efec70cc6e67a0e6306
zhongzhengmin <zhongzhengmin@lvmama.com>
2020-05-14 11:50:54 +0800

    1

---
+++ src/main/java/org/didinem/p75/repeat/S05141.java
@@ -0,0 +1,66 @@
+package org.didinem.p75.repeat;
+
+import java.util.Arrays;
+
+/**
+ * @Classname S05141
+ * @Description TODO
+ * @Date 2020/5/14 11:42
+ * @Created by didinem
+ */
+public class S05141 {
+
+    public static void sortColors(int[] nums) {
+        sort0(nums, 0, nums.length - 1);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public static void sort0(int[] nums, int leftIndex, int rightIndex) {
+        if (nums == null) {
+            return;
+        }
+        if (leftIndex >= rightIndex) {
+            return;
+        }
+
+        int i = leftIndex;
+        int j = rightIndex;
+        int temp;
+        while (i < j) {
+            while (i < j) {
+                if (nums[i] > nums[j]) {
+                    temp = nums[j];
+                    nums[j] = nums[i];
+                    nums[i] = temp;
+                    i++;
+                    break;
+                }
+                j--;
+            }
+            while (i < j) {
+                if (nums[i] > nums[j]) {
+                    temp = nums[j];
+                    nums[j] = nums[i];
+                    nums[i] = temp;
+                    j--;
+                    break;
+                }
+                i++;
+            }
+        }
+
+        if ((i - 1) - leftIndex > 0) {
+            sort0(nums, leftIndex, i - 1);
+        }
+        if (rightIndex - (i + 1) > 0) {
+            sort0(nums, (i + 1), rightIndex);
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        sortColors(new int[]{3, 5, 1, 2, 7, 5, 4});
+    }
+
+}
